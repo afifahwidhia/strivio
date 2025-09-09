06e579d61eb0c5f87d7d1eb767b3bc974052b8c6
afifahwidhia <afifahwidhiaa@gmail.comgit config --global user.email afifahwidhiaa@gmail.com>
2025-09-09 17:08:16 +0700

    Update Tugas 2

---
+++ main/models.py
@@ -0,0 +1,33 @@
+import uuid
+from django.db import models
+
+class News(models.Model):
+    CATEGORY_CHOICES = [
+        ('transfer', 'Transfer'),
+        ('update', 'Update'),
+        ('exclusive', 'Exclusive'),
+        ('match', 'Match'),
+        ('rumor', 'Rumor'),
+        ('analysis', 'Analysis'),
+    ]
+    
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    thumbnail = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    is_featured = models.BooleanField(default=False)
+    rating = models.FloatField(default=0)
+    stock = models.PositiveIntegerField(default=0)
+    size = models.PositiveIntegerField(default=38)
+    
+    def __str__(self):
+        return self.title
+    
+    @property
+    def is_news_hot(self):
+        return self.news_views > 20
+        
+    def increment_views(self):
+        self.news_views += 1
+        self.save()
